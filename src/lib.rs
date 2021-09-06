a9a4479f7d4b0cdea9603a22c8c3de5a0c14c2f4
Lucas V. Schuermann <lucas@lvs.io>
2021-09-06 15:59:15 -0400

    initial working example of wasm

---
+++ src/lib.rs
@@ -0,0 +1,201 @@
+#![warn(
+    unreachable_pub,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    rust_2018_idioms,
+    missing_debug_implementations
+)]
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader};
+
+pub mod solver;
+
+const DAM_PARTICLES: usize = 1000;
+const BLOCK_PARTICLES: usize = 500;
+const POINT_SIZE: f32 = 5.0;
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let context = canvas
+        .get_context("webgl2")?
+        .unwrap()
+        .dyn_into::<WebGl2RenderingContext>()?;
+
+    let vert_shader = compile_shader(
+        &context,
+        WebGl2RenderingContext::VERTEX_SHADER,
+        format!(
+            r##"#version 300 es
+        uniform mat4 matrix;
+        in vec2 position;
+        void main() {{
+            gl_PointSize = {:.1};
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
+        }}
+        "##,
+            POINT_SIZE
+        )
+        .as_str(),
+    )?;
+
+    let frag_shader = compile_shader(
+        &context,
+        WebGl2RenderingContext::FRAGMENT_SHADER,
+        r##"#version 300 es
+        precision highp float;
+        out vec4 outColor;
+        void main() {{
+            outColor = vec4(0.2, 0.6, 1.0, 1.0);
+        }}
+        "##,
+    )?;
+    let program = link_program(&context, &vert_shader, &frag_shader)?;
+    context.use_program(Some(&program));
+
+    // uniforms
+    let uniform_location = context
+        .get_uniform_location(&program, "matrix")
+        .expect("Unable to get shader projection matrix uniform location");
+    let ortho_matrix = cgmath::ortho(0.0, solver::VIEW_WIDTH, 0.0, solver::VIEW_HEIGHT, 0.0, 1.0);
+    let ortho_matrix_flattened_ref: &[f32; 16] = ortho_matrix.as_ref();
+    context.uniform_matrix4fv_with_f32_array(
+        Some(&uniform_location),
+        false,
+        ortho_matrix_flattened_ref,
+    );
+
+    // attributes
+    let position_attribute_location = context.get_attrib_location(&program, "position");
+    let buffer = context.create_buffer().ok_or("Failed to create buffer")?;
+    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
+    context.vertex_attrib_pointer_with_i32(0, 2, WebGl2RenderingContext::FLOAT, false, 0, 0);
+    context.enable_vertex_attrib_array(position_attribute_location as u32);
+
+    // TODO: move
+    let mut sim = solver::State::new();
+    sim.init_dam_break(DAM_PARTICLES);
+
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
+
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        //         if i > 300 {
+        //             body().set_text_content(Some("All done!"));
+        //
+        //             // Drop our handle to this closure so that it will get cleaned
+        //             // up once we return.
+        //             let _ = f.borrow_mut().take();
+        //             return;
+        //         }
+
+        sim.update();
+        let vertices: Vec<f32> = sim
+            .particles
+            .iter()
+            .map(|p| p.position().to_array())
+            .flatten()
+            .collect();
+        // Note that `Float32Array::view` is somewhat dangerous (hence the
+        // `unsafe`!). This is creating a raw view into our module's
+        // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
+        // (aka do a memory allocation in Rust) it'll cause the buffer to change,
+        // causing the `Float32Array` to be invalid.
+        //
+        // As a result, after `Float32Array::view` we have to be very careful not to
+        // do any memory allocations before it's dropped.
+        unsafe {
+            let positions_array_buf_view = js_sys::Float32Array::view(&vertices);
+
+            context.buffer_data_with_array_buffer_view(
+                WebGl2RenderingContext::ARRAY_BUFFER,
+                &positions_array_buf_view,
+                WebGl2RenderingContext::DYNAMIC_DRAW,
+            );
+        }
+
+        let vert_count = (vertices.len() / 2) as i32;
+        draw(&context, vert_count);
+
+        // Schedule ourself for another requestAnimationFrame callback.
+        request_animation_frame(f.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut()>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
+
+    Ok(())
+}
+
+fn draw(context: &WebGl2RenderingContext, vert_count: i32) {
+    context.clear_color(0.0, 0.0, 0.0, 1.0);
+    context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+    context.draw_arrays(WebGl2RenderingContext::POINTS, 0, vert_count);
+}
+
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+fn compile_shader(
+    context: &WebGl2RenderingContext,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = context
+        .create_shader(shader_type)
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+    context.shader_source(&shader, source);
+    context.compile_shader(&shader);
+
+    if context
+        .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(shader)
+    } else {
+        Err(context
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+    }
+}
+
+fn link_program(
+    context: &WebGl2RenderingContext,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
+    let program = context
+        .create_program()
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+
+    context.attach_shader(&program, vert_shader);
+    context.attach_shader(&program, frag_shader);
+    context.link_program(&program);
+
+    if context
+        .get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(program)
+    } else {
+        Err(context
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| String::from("Unknown error creating program object")))
+    }
+}
---
+++ src/solver.rs
@@ -0,0 +1,249 @@
+use glam::{vec2, vec3, UVec2, Vec2, Vec3};
+//use rayon::prelude::*;
+use std::f32::consts::PI;
+//pub use wasm_bindgen_rayon::init_thread_pool;
+
+pub const G: Vec2 = glam::const_vec2!([0.0, -9.81]);
+pub const WINDOW_WIDTH: u32 = 1280;
+pub const WINDOW_HEIGHT: u32 = 800;
+pub const VIEW_WIDTH: f32 = 20.0;
+pub const VIEW_HEIGHT: f32 = WINDOW_HEIGHT as f32 * VIEW_WIDTH / WINDOW_WIDTH as f32;
+
+const SOLVER_STEPS: usize = 10;
+const REST_DENS: f32 = 45.0;
+const STIFFNESS: f32 = 0.08;
+const STIFF_APPROX: f32 = 0.1;
+const SURFACE_TENSION: f32 = 0.0001;
+const LINEAR_VISC: f32 = 0.25;
+const QUAD_VISC: f32 = 0.5;
+const PARTICLE_RADIUS: f32 = 0.03;
+const H: f32 = 6.0 * PARTICLE_RADIUS;
+const H2: f32 = H * H;
+const DT: f32 = (1.0 / 40.0) / SOLVER_STEPS as f32;
+const DT2: f32 = DT * DT;
+const KERN: f32 = 20.0 / (2.0 * PI * H * H);
+const KERN_NORM: f32 = 30.0 / (2.0 * PI * H * H);
+const EPS: f32 = 0.0000001;
+const EPS2: f32 = EPS * EPS;
+
+const CELL_SIZE: f32 = H; // set to smoothing radius
+const GRID_WIDTH: usize = (VIEW_WIDTH / CELL_SIZE) as usize;
+const GRID_HEIGHT: usize = (VIEW_HEIGHT / CELL_SIZE) as usize;
+const NUM_CELLS: usize = GRID_WIDTH * GRID_HEIGHT;
+const NUM_NEIGHBORS: usize = 64;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Particle {
+    x: Vec2,
+    xlast: Vec2,
+    v: Vec2,
+    m: f32,
+    p: f32,
+    pv: f32,
+    d: f32,
+    dv: f32,
+    grid_index: UVec2,
+}
+
+impl Particle {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self {
+            x: Vec2::new(x, y),
+            m: 1.0,
+            ..Default::default()
+        }
+    }
+
+    #[inline(always)]
+    pub fn position(&self) -> Vec2 {
+        self.x
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct State {
+    pub particles: Vec<Particle>,
+    boundaries: [Vec3; 4],
+    grid: Vec<Vec<usize>>,
+    neighborhoods: Vec<Vec<Neighbor>>,
+}
+
+#[derive(Debug)]
+struct Neighbor {
+    index: usize,
+    r: f32,
+}
+
+impl Neighbor {
+    fn new(index: usize, r: f32) -> Self {
+        Neighbor { index, r }
+    }
+}
+
+impl State {
+    pub fn new() -> Self {
+        let boundaries = [
+            vec3(1.0, 0.0, 0.0),           // left
+            vec3(0.0, 1.0, 0.0),           // bottom
+            vec3(-1.0, 0.0, -VIEW_WIDTH),  // right
+            vec3(0.0, -1.0, -VIEW_HEIGHT), // top
+        ];
+        let grid = vec![vec![]; NUM_CELLS];
+        Self {
+            boundaries,
+            grid,
+            ..Default::default()
+        }
+    }
+
+    fn place_square(&mut self, start: &mut Vec2, max_particles: usize) -> usize {
+        let x0 = start.x;
+        let num = f32::sqrt(max_particles as f32) as usize;
+        for _ in 0..num {
+            for _ in 0..num {
+                self.particles.push(Particle::new(start.x, start.y));
+                self.neighborhoods.push(vec![]);
+                start.x += 2.0 * PARTICLE_RADIUS + PARTICLE_RADIUS;
+            }
+            start.x = x0;
+            start.y -= 2.0 * PARTICLE_RADIUS + PARTICLE_RADIUS;
+        }
+        num * num
+    }
+
+    pub fn init_dam_break(&mut self, dam_max_particles: usize) {
+        let mut start = vec2(0.25 * VIEW_WIDTH, 0.95 * VIEW_HEIGHT);
+        self.place_square(&mut start, dam_max_particles);
+        /*info!(
+            "Initialized dam break with {} particles",
+            self.particles.len()
+        );*/
+    }
+
+    pub fn init_block(&mut self, block_max_particles: usize) {
+        let mut start = vec2(
+            VIEW_WIDTH / 2.0 - VIEW_HEIGHT / 10.0,
+            VIEW_HEIGHT - VIEW_HEIGHT / 10.0,
+        );
+        let num_placed = self.place_square(&mut start, block_max_particles);
+        /*info!(
+            "Initialized block of {} particles, new total {}",
+            num_placed,
+            self.particles.len()
+        );*/
+    }
+
+    fn integrate_insert(&mut self) {
+        let grid = &mut self.grid;
+        grid.iter_mut().for_each(|g| g.clear());
+        self.particles.iter_mut().enumerate().for_each(|(i, p)| {
+            p.v += G * DT;
+            p.xlast = p.x;
+            p.x += DT * p.v;
+
+            let xind = (p.x.x / CELL_SIZE).floor() as usize;
+            let yind = (p.x.y / CELL_SIZE).floor() as usize;
+            let xind = usize::max(1, usize::min(GRID_WIDTH - 2, xind));
+            let yind = usize::max(1, usize::min(GRID_HEIGHT - 2, yind));
+            grid[xind + yind * GRID_WIDTH].push(i);
+            p.grid_index = UVec2::new(xind as u32, (yind * GRID_WIDTH) as u32);
+        });
+    }
+
+    fn compute_forces(&mut self) {
+        // TODO can we get around this clone
+        let particles_initial = self.particles.clone();
+        let grid = &self.grid;
+        self.particles
+            .iter_mut()
+            .zip(self.neighborhoods.iter_mut())
+            .for_each(|(pi, ni)| {
+                ni.clear();
+                let mut dens = 0.0;
+                let mut dens_proj = 0.0;
+                for gx in (pi.grid_index.x - 1)..=(pi.grid_index.x + 1) {
+                    let y_range = (pi.grid_index.y - GRID_WIDTH as u32)
+                        ..=(pi.grid_index.y + GRID_WIDTH as u32);
+                    for gy in y_range.step_by(GRID_WIDTH) {
+                        for j in &grid[(gx + gy) as usize] {
+                            let pj = particles_initial[*j];
+                            let dx = pj.x - pi.x;
+                            let r2 = dx.length_squared();
+                            if !(EPS2..=H2).contains(&r2) {
+                                continue;
+                            }
+                            let r = f32::sqrt(r2);
+                            let a = 1.0 - r / H;
+                            dens += pj.m * a * a * a * KERN;
+                            dens_proj += pj.m * a * a * a * a * KERN_NORM;
+                            if ni.len() < NUM_NEIGHBORS {
+                                let neighbor = Neighbor::new(*j, r);
+                                ni.push(neighbor);
+                            }
+                        }
+                    }
+                }
+                pi.d = dens;
+                pi.dv = dens_proj;
+                pi.p = STIFFNESS * (dens - pi.m * REST_DENS);
+                pi.pv = STIFF_APPROX * dens_proj;
+            })
+    }
+
+    fn project_correct(&mut self) {
+        // TODO can we get around this clone
+        let particles_initial = self.particles.clone();
+        let bounds = self.boundaries;
+        self.particles
+            .iter_mut()
+            .zip(self.neighborhoods.iter_mut())
+            .for_each(|(pi, ni)| {
+                // project
+                let mut xproj = pi.x;
+                for neighbor in ni {
+                    let pj = particles_initial[neighbor.index];
+                    let r = neighbor.r;
+                    let dx = pj.x - pi.x;
+                    let a = 1.0 - r / H;
+                    let d = DT2
+                        * ((pi.pv + pj.pv) * a * a * a * KERN_NORM + (pi.p + pj.p) * a * a * KERN)
+                        / 2.0;
+
+                    // relaxation
+                    xproj -= d * dx / (r * pi.m);
+
+                    // surface tension
+                    xproj += (SURFACE_TENSION / pi.m) * pj.m * a * a * KERN * dx;
+
+                    // linear and quadratic visc
+                    let dv = pi.v - pj.v;
+                    let mut u = dv.dot(dx);
+                    if u > 0.0 {
+                        u /= r;
+                        let big_i = 0.5 * DT * a * (LINEAR_VISC * u + QUAD_VISC * u * u);
+                        xproj -= big_i * dx * DT;
+                    }
+                }
+
+                // correct
+                pi.x = xproj;
+                pi.v = (xproj - pi.xlast) / DT;
+
+                // boundary
+                for b in &bounds {
+                    let d = f32::max(pi.x.x * b.x + pi.x.y * b.y - b.z, 0.0);
+                    if d < PARTICLE_RADIUS {
+                        pi.v += (PARTICLE_RADIUS - d) * Vec2::new(b.x, b.y) / DT;
+                    }
+                }
+            })
+    }
+
+    pub fn update(&mut self) {
+        for _ in 0..SOLVER_STEPS {
+            self.integrate_insert();
+            self.compute_forces();
+            self.project_correct();
+        }
+    }
+}
