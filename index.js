e9df453244efaa3b3333893595fc89c1d73febbb
Lucas V. Schuermann <lucas@lvs.io>
2021-11-07 11:57:57 -0500

    cleanup demo frontend

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ import { threads } from 'wasm-feature-detect';
 import * as Stats from 'stats.js';
 
 (async () => {
+    const $ = (id) => document.getElementById(id);
+
     // check if required features are supported, else show error and exit
     if (!(await threads()) || !HTMLCanvasElement.prototype.transferControlToOffscreen) {
         const errorString = 'Browser does not support required features';
         console.error(errorString);
-        const canvas = document.getElementById('canvas');
-        canvas.style = 'border: 1px solid red';
-        const ctx = canvas.getContext('2d');
+        const ctx = $('canvas').getContext('2d');
         ctx.font = '16px serif';
-        ctx.fillText(errorString, 10, 25);
+        ctx.fillText(errorString, 0, 16);
         return;
     }
 
@@ -25,21 +25,21 @@ import * as Stats from 'stats.js';
     // attach perf stats window
     const stats = new Stats();
     stats.dom.style.position = 'absolute';
-    document.getElementById('stats').appendChild(stats.dom);
+    $('stats').appendChild(stats.dom);
 
-    const numThreads = await handlers.numThreads;
-    const setInfo = (numParticles) => document.getElementById('info').innerText = `Threads: ${numThreads}, Particles: ${numParticles}`;
+    $('threads').innerText = await handlers.numThreads;
+    const setInfo = (numParticles) => $('count').innerText = numParticles;
 
     // create offscreen canvas, pass to worker, and start WASM sim+render loop in worker
-    const offscreenCanvas = document.getElementById('canvas').transferControlToOffscreen();
+    const offscreenCanvas = $('canvas').transferControlToOffscreen();
     const numParticles = await handlers.init(Comlink.transfer(offscreenCanvas, [offscreenCanvas]), Comlink.proxy(stats));
     setInfo(numParticles);
 
     // bind interactivity
-    document.getElementById('block').addEventListener('click', async () => {
+    $('block').onclick = async () => {
         setInfo(await handlers.addBlock());
-    });
-    document.getElementById('reset').addEventListener('click', async () => {
+    };
+    $('reset').onclick = async () => {
         setInfo(await handlers.reset());
-    });
+    };
 })();
